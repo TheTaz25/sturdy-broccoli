7606197af334fdd6e42ac67ee4191d347d119ce3
Denis Ergin <thetazlp@hotmail.de>
2020-06-17 21:21:40 +0200

    Basic Project Setup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import scss from 'rollup-plugin-scss';
+import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+
+import pkg from './package.json';
+
+export default {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+			sourcemap: true
+		},
+		{
+			file: pkg.module,
+			format: 'esm',
+			sourcemap: true
+		}
+	],
+	plugins: [
+		peerDepsExternal(),
+		resolve(),
+		commonjs(),
+		typescript({ useTsconfigDeclarationDir: true }),
+		scss()
+	]
+};
+
