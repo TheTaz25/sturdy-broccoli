a3e0fac31d86602191919126d00eaa4ed7981f89
Denis Ergin <thetazlp@hotmail.de>
2020-06-23 19:39:06 +0200

    Small function for storybook to generate text

---
+++ src/storobookUtils/textGenerator.ts
@@ -0,0 +1,9 @@
+export default function generateTextFiveTimes() {
+  return `
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptatibus cum voluptates ea? Asperiores vitae officiis minus commodi ex nemo voluptatibus, soluta debitis, doloribus modi numquam facere repudiandae inventore sapiente?
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis aspernatur voluptate soluta. Accusamus alias unde ea, voluptates minima facilis in beatae, error quos perspiciatis reprehenderit sequi aliquam hic praesentium aperiam.
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati perspiciatis autem odit doloremque debitis voluptates? Reprehenderit tempora vel voluptates asperiores consectetur quibusdam recusandae, doloribus quod quas magnam repellendus odit ex!
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, nam. Saepe officiis pariatur nulla ducimus iure possimus reiciendis autem quis distinctio modi beatae, cumque temporibus nam accusamus, sed et totam.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ut officia modi veniam minus dignissimos inventore est, similique quaerat doloremque nesciunt quam dolore reiciendis quas expedita quos, distinctio nihil ullam.
+  `;
+}
