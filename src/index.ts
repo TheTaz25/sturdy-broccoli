02e71c10b63ba007f93a2a374dfb68a4e58e5e36
Denis Ergin <thetazlp@hotmail.de>
2020-07-20 07:43:03 +0200

    Added new Components to index file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import Base from './Base/Base';
 import Paragraph from './Paragraph/Paragraph';
 import Headline from './Headline/Headline';
+import Quote from './Quote/Quote';
+import Space from './Space/Space';
+import Subtitle from './Subtitle/Subtitle';
+import Text from './Text/Text';
 
 export {
   Base,
   Paragraph,
   Headline,
+  Quote,
+  Space,
+  Subtitle,
+  Text,
 };
