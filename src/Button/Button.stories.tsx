a671ea4da748057a06a10e3089957538312d853b
Denis Ergin <thetazlp@hotmail.de>
2020-08-31 07:54:31 +0200

    Made the button story for all Options

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import Button from './Button';
 import Base from '../Base/Base';
+import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+import { ButtonTypes, ButtonColors, ButtonEffects } from '../defaultInterfaces';
 
 export default {
   title: 'Button',
   component: Button,
+  decorators: [ withKnobs ],
 };
 
-export const Basic = () => (
-  <Base>
-    <Button color="primary">My Button</Button>
-  </Base>
-);
+const colorOptions = {
+  Default: 'default',
+  Primary: 'primary',
+  Success: 'success',
+  Warn: 'warn',
+  Danger: 'danger',
+  Info: 'info'
+};
+
+const effectOptions = {
+  Elevated: 'elevated',
+  Clicky: 'clicky',
+  None: undefined,
+};
+
+const intentionOptions = {
+  CTA: 'cta',
+  Primary: 'primary',
+  Outlined: 'outlined'
+};
+
+export const AllSettings = () => {
+  const disabled = boolean('Disabled', false);
+  const intention = select('Intention (Variation)', intentionOptions, intentionOptions.Outlined);
+  const color = select('Colors', colorOptions, colorOptions.Default);
+  const circle = boolean('Style as circle', false);
+  const roundedCorners = boolean('Rounded Corners', false);
+  const effect = select('Effect', effectOptions, effectOptions.None);
+
+  return (
+    <Base>
+      <Button
+        disabled={disabled}
+        circle={circle}
+        roundedCorners={roundedCorners}
+        color={color as ButtonColors}
+        intention={intention as ButtonTypes}
+        effect={effect as ButtonEffects}
+      >
+        My Button
+      </Button>
+    </Base>
+  );
+};
