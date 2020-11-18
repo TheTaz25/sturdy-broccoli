1027e242ec40cce2b4c5c110083a9bdfa4d8da26
Ergin Denis <denis.ergin@outlook.de>
2020-11-18 22:21:29 +0100

    Updated package deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,16 @@
 const path = require('path');
 
 module.exports = {
-	stories: ['../src/**/*.stories.tsx'],
-	// Add any storybook addons
-	addons: [
-		'@storybook/addon-storysource',
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
+    '@storybook/addon-storysource',
 		'@storybook/addon-knobs/register',
 		'@storybook/addon-viewport/register',
-	],
-	webpackFinal: async (config) => {
-		config.module.rules.push({
-			test: /\.scss$/,
-			use: ['style-loader', 'css-loader', 'sass-loader'],
-			include: path.resolve(__dirname, '../')
-		});
-
-		config.module.rules.push({
-			test: /\.(ts|tsx)$/,
-			loader: require.resolve('babel-loader'),
-			options: {
-				presets: [['react-app', { flow: false, typescript: true }]]
-			}
-		});
-		config.resolve.extensions.push('.ts', '.tsx');
-
-		return config;
-	}
+  ],
 };
-
